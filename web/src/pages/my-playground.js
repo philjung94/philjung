1d1f1be1a2cb7f62dbb230d4fe7e92f94bb4ba3b
Phil Jung <seokhyun.phil.jung@gmail.com>
2019-08-19 22:59:33 +1000

    Added my old playcanvas game as a playground placeholder

--- web/src/pages/my-playground.js
+++ web/src/pages/my-playground.js
@@ -3,12 +3,15 @@ import Layout from "../components/layout"
 import Stars from "../images/c-img__stars.svg"
 import Divider from "../components/divider"
 import Intro from "../components/intro"
-
+import Segment from "../components/segment"
 export default function MyPlayground() {
   return (
     <Layout bg={Stars}>
       <Intro/>
       <Divider height="9rem"/>
+      <Segment>
+        <iframe width="1000px" height="600px" title="Game" src="https://playcanv.as/p/KEW3puu7/"/>
+      </Segment>
     </Layout>
   )
 }
